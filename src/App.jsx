c0f710ebfe482ef3e9c5be92d51f0f883f345902
ZeVictorAraujo <josearaujo.ba@hotmail.com>
2024-05-22 19:15:46 -0300

    Design e layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { toast, ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import { FaGithub } from 'react-icons/fa';
 import Registrar from './registrar';
 import Visualizar from './visualizar';
 
@@ -22,13 +23,18 @@ function Principal() {
     const pessoa = { nome, rg, cpf, nasc, email, telefone };
 
     try {
-      await fetch('http://localhost:3000/pessoa', {
+      const response = await fetch('http://localhost:3000/pessoa', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json'
         },
         body: JSON.stringify(pessoa)
       });
+
+      if (!response.ok) {
+        throw new Error('Erro ao registrar pessoa');
+      }
+
       // Limpar os campos de entrada
       setNome('');
       setRg('');
@@ -36,6 +42,7 @@ function Principal() {
       setNasc('');
       setEmail('');
       setTelefone('');
+
       // Exibir mensagem de confirmação
       toast.success('Registro realizado com sucesso!', {
         position: "top-right"
@@ -49,35 +56,42 @@ function Principal() {
   };
 
   return (
-    <div className="container">
-      <div className="botao-container">
-        <button onClick={() => cliqueSecao('visualizar')} className='botao-menu' id="muni">Visualizar</button>
-        <button onClick={() => cliqueSecao('registrar')} className='botao-menu' id="regi">Registrar</button>
+    <div className="main-container">
+      <div className="left-container">
+        <img src="logo.png" alt="Brasao do Estado" className="flag" />
+        <h2>Sistema de Registros de Municipes</h2><br/>
+        <p>Sistema desenvolvido para modernizar o processo de cadastramento de munícipes, evitando duplicidades e aumentando a eficiência.</p>
+        <p className='creditos'>Desenvolvedor <a href='https://github.com/TDSVictor'>Victor Araujo <FaGithub/></a></p>
       </div>
+      <div className="divider"></div>
+      <div className="right-container">
+        <div className="botao-container">
+          <button onClick={() => cliqueSecao('visualizar')} className='botao-menu' id="muni">Visualizar</button>
+          <button onClick={() => cliqueSecao('registrar')} className='botao-menu' id="regi">Registrar</button>
+        </div>
 
-      <div className="visualizar">
         {secaoAtual === 'visualizar' && (
-          <>
-          <Visualizar />
-          <ToastContainer />
-          </>
+          <div className="visualizar">
+            <Visualizar />
+            <ToastContainer />
+          </div>
+        )}
+
+        {secaoAtual === 'registrar' && (
+          <div className="registrar">
+            <Registrar 
+              nome={nome} setNome={setNome}
+              rg={rg} setRg={setRg}
+              cpf={cpf} setCpf={setCpf}
+              nasc={nasc} setNasc={setNasc}
+              email={email} setEmail={setEmail}
+              telefone={telefone} setTelefone={setTelefone}
+              handleSubmit={handleSubmit}
+            />
+            <ToastContainer />
+          </div>
         )}
       </div>
-
-      {secaoAtual === 'registrar' && (
-        <div>
-          <Registrar 
-            nome={nome} setNome={setNome}
-            rg={rg} setRg={setRg}
-            cpf={cpf} setCpf={setCpf}
-            nasc={nasc} setNasc={setNasc}
-            email={email} setEmail={setEmail}
-            telefone={telefone} setTelefone={setTelefone}
-            handleSubmit={handleSubmit}
-          />
-          <ToastContainer />
-        </div>
-      )}
     </div>
   );
 }
